80c8c063a257ae4b065790418d40c653d4390015
Andrew <atg27@uw.edu>
2021-03-07 14:30:49 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names = ['Lisa', 'Kaitlin', 'Jan'];
+const event = 'surprise';
+
+function writeCards(names, event){
+    let newArray = [];
+    for (let i = 0; i < names.length; i++){
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray
+}
+function countDown (number = 10){
+    while (number >= 0) {
+      console.log(number--);
+    }
+}
+    
+
